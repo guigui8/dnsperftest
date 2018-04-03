c48f8e542b6f888789578b18d2ff21a8c46f362e
Guilhem Chaminas <guilhem.chaminas@eisti.fr>
2018-04-03 19:42:28 +0200

    dns fdn, domaines, timeout, pas de details

--- dnstest.sh
+++ dnstest.sh
@@ -3,53 +3,51 @@
 command -v bc > /dev/null || { echo "bc was not found. Please install bc."; exit 1; }
 { command -v drill > /dev/null && dig=drill; } || { command -v dig > /dev/null && dig=dig; } || { echo "dig was not found. Please install dnsutils."; exit 1; }
 
-
-
-NAMESERVERS=`cat /etc/resolv.conf |grep nameserver | cut -d " " -f 2 | sed 's/\(.*\)/&#&/'`
-
 PROVIDERS="
-1.1.1.1#cloudflare 
-8.8.8.8#google 
-9.9.9.9#quad9 
-208.67.222.123#opendns 
-199.85.126.20#norton 
-185.228.168.168#cleanbrowsing 
-77.88.8.7#yandex 
-176.103.130.132#adguard 
-156.154.70.3#neustar 
+1.1.1.1#cloudflare
+1.0.0.1#cloudflare2nd
+8.8.8.8#google
+8.8.4.4#google2nd
+9.9.9.9#quad9
+208.67.222.123#opendns
+199.85.126.20#norton
+185.228.168.168#cleanbrowsing
+77.88.8.7#yandex
+176.103.130.132#adguard
+156.154.70.3#neustar
 8.26.56.26#comodo
+80.67.169.12#fdn1
+80.67.169.40#fdn2
 "
 
 # Domains to test. Duplicated domains are ok
-DOMAINS2TEST="www.google.com amazon.com facebook.com www.youtube.com www.reddit.com  wikipedia.org twitter.com gmail.com www.google.com whatsapp.com"
+DOMAINS2TEST="www.google.com amazon.com facebook.com www.youtube.com www.reddit.com  wikipedia.org twitter.com gmail.com www.google.com whatsapp.com lafibre.info nextinpact.com arel.eisti.fr eisti.fr www.humblebundle.com www.hardware.fr"
 
 
 totaldomains=0
-printf "%-18s" ""
+# printf "%-15s" ""
 for d in $DOMAINS2TEST; do
     totaldomains=$((totaldomains + 1))
-    printf "%-8s" "test$totaldomains"
+    # printf "%-8s" "test$totaldomains"
 done
 printf "%-8s" "Average"
 echo ""
 
 
-for p in $NAMESERVERS $PROVIDERS; do
+for p in $PROVIDERS; do
     pip=${p%%#*}
     pname=${p##*#}
     ftime=0
 
-    printf "%-18s" "$pname"
+    printf "%-15s" "$pname"
     for d in $DOMAINS2TEST; do
-        ttime=`$dig +tries=1 +time=2 +stats @$pip $d |grep "Query time:" | cut -d : -f 2- | cut -d " " -f 2`
-        if [ -z "$ttime" ]; then
-	        #let's have time out be 1s = 1000ms
-	        ttime=1000
-        elif [ "x$ttime" = "x0" ]; then
-	        ttime=1
-	    fi
-
-        printf "%-8s" "$ttime ms"
+        ttime=`$dig +stats +time=1 +tries=2 @$pip $d |grep "Query time:" | cut -d : -f 2- | cut -d " " -f 2`
+	if [ -z "$ttime" ]; then
+	    #let's have time out be 10s = 10000ms
+	    ttime=10000
+      # break
+	fi
+        # printf "%-8s" "$ttime ms"
         ftime=$((ftime + ttime))
     done
     avg=`bc -lq <<< "scale=2; $ftime/$totaldomains"`
